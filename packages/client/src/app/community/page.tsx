b38e378261e14c2d1d2e3a374a0e7da6a14719a2
KIMTAEKJU <gkffntm12@naver.com>
2023-04-12 02:00:22 +0900

    feat: 인기태그 퍼블리싱 완료

--- packages/client/src/app/community/page.tsx
+++ packages/client/src/app/community/page.tsx
@@ -1,24 +1,78 @@
 function Community() {
   return (
     <div className="h-[calc(100vh_-_16.563rem)] w-full flex justify-center items-center">
-      <div className="w-full h-full pt-10 pb-40 pl-180 pr-180" >
+      <div className="w-full h-full pt-10 pb-40 pl-180 pr-180">
         <div className="grid grid-cols-community-main grid-rows-community-main w-full h-full gap-x-10">
           <div>
-            <div className="h-220 bg-white border rounded-lg p-5">
-              <div className="border-b-2 border-black">
+            <div className="h-220 bg-white border rounded-lg p-5 flex flex-col gap-y-4">
+              <h3 className="font-pretendard not-italic font-bold text-18 leading-21 text-text-color">
                 인기 태그
-              </div>
-              <div>
-                JavaScript
+              </h3>
+              <div className="border border-border-color" />
+              <div className="flex flex-col gap-2">
+                <div className="flex justify-between w-full">
+                  <h4 className="font-pretendard not-italic font-normal text-16 leading-19">
+                    JavaScript
+                  </h4>
+                  <div className="w-27 h-18">
+                    <h5 className="font-pretendard not-italic font-medium text-12 leading-14 px-1.5 py-0.5 border rounded-40 bg-number-grey">
+                      56
+                    </h5>
+                  </div>
+                </div>
+
+                <div className="flex justify-between w-full">
+                  <h4 className="font-pretendard not-italic font-normal text-16 leading-19 text-number-grey">
+                    JQuery
+                  </h4>
+                  <div className="w-27 h-18">
+                    <h5 className="font-pretendard not-italic font-medium text-12 leading-14 px-1.5 py-0.5 border rounded-40 bg-number-real-grey text-number-grey">
+                      47
+                    </h5>
+                  </div>
+                </div>
+
+                <div className="flex justify-between w-full">
+                  <h4 className="font-pretendard not-italic font-normal text-16 leading-19 text-number-grey">
+                    React
+                  </h4>
+                  <div className="w-27 h-18">
+                    <h5 className="font-pretendard not-italic font-medium text-12 leading-14 px-1.5 py-0.5 border rounded-40 bg-number-real-grey text-number-grey">
+                      36
+                    </h5>
+                  </div>
+                </div>
+
+                <div className="flex justify-between w-full">
+                  <h4 className="font-pretendard not-italic font-normal text-16 leading-19 text-number-grey">
+                    Linux
+                  </h4>
+                  <div className="w-27 h-18">
+                    <h5 className="font-pretendard not-italic font-medium text-12 leading-14 px-1.5 py-0.5 border rounded-40 bg-number-real-grey text-number-grey">
+                      23
+                    </h5>
+                  </div>
+                </div>
+
+                <div className="flex justify-between w-full">
+                  <h4 className="font-pretendard not-italic font-normal text-16 leading-19 text-number-grey">
+                    Python
+                  </h4>
+                  <div className="w-27 h-18">
+                    <h5 className="font-pretendard not-italic font-medium text-12 leading-14 px-1.5 py-0.5 border rounded-40 bg-number-real-grey text-number-grey">
+                      21
+                    </h5>
+                  </div>
+                </div>
               </div>
             </div>
             <div>22</div>
           </div>
           <div>33</div>
-        </div>    
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Community;
--- packages/client/tailwind.config.cjs
+++ packages/client/tailwind.config.cjs
@@ -1,25 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      width: {
+        27: "1.688rem",
+      },
       height: {
-        "100": "6.25rem",
-        "165": "10.313rem",
-        "220": "13.75rem",
+        18: "1.125rem",
+        100: "6.25rem",
+        165: "10.313rem",
+        220: "13.75rem",
       },
       padding: {
-        "180": "11.25rem",
+        180: "11.25rem",
       },
       gridTemplateColumns: {
-        'community-main': '14.75rem 82.2%',
+        "community-main": "14.75rem 82.2%",
       },
       gridTemplateRows: {
-        'community-main': '100%'
-      }
+        "community-main": "100%",
+      },
+      fontFamily: {
+        pretendard: "Pretendard",
+      },
+      fontSize: {
+        12: "0.75rem",
+        18: "1.125rem",
+        16: "1rem",
+      },
+      lineHeight: {
+        14: "0.875rem",
+        19: "1.188rem",
+        21: "1.313rem",
+      },
+      colors: {
+        "text-color": "#222222",
+        "border-color": "#DDDDDD",
+        "number-grey": "#999999",
+        "number-real-grey": "F8F8F8",
+      },
+      borderRadius: {
+        40: "2.5rem",
+      },
     },
   },
   plugins: [],
-}
+};
