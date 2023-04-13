8e823425ab5223c44bc47fd9c18bebcef130b1b7
CHOHYUNHWA <gusghk115@gmail.com>
2023-04-14 00:04:25 +0900

    fix:appspec.yml

--- script/server_start.sh
+++ script/server_start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ubuntu/build
-sudo nohup java -jar server-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+#sudo nohup java -jar server-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+sudo nohup java -jar server-SNAPSHOT.jar &
