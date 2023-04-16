69b1566403e54aff07b1bad2472ebac3dafd602b
yoohyunju <yhj2756@naver.com>
2023-04-16 23:39:02 +0900

    feat: footer 마크업 (#7)

--- packages/client/src/components/common/footer.tsx
+++ packages/client/src/components/common/footer.tsx
@@ -1,5 +1,19 @@
 function Footer() {
-  return <div>FOOTER</div>;
+  return (
+    <footer>
+      <div>
+        <div>Doffy Logo</div>
+        <div>Copyright ⓒ2023 all rights reserved</div>
+      </div>
+      <div>
+        <ul>
+          <li>이용약관</li>
+          <li>개인정보처리방침</li>
+        </ul>
+      </div>
+      <div>Get In Touch With Us</div>
+    </footer>
+  );
 }
 
 export default Footer;
