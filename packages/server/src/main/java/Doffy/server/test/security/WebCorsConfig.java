e56fe6d16817f65179af347adcf6ea818449641a
CHOHYUNHWA <gusghk115@gmail.com>
2023-04-26 00:12:20 +0900

    test : cors 설정 코드 추

--- packages/server/src/main/java/Doffy/server/test/security/WebCorsConfig.java
+++ packages/server/src/main/java/Doffy/server/test/security/WebCorsConfig.java
@@ -1,18 +1,18 @@
-//package Doffy.server.test.security;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebCorsConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry){
-//        registry.addMapping("/**")
-//                .allowCredentials(true)
-//                .allowedOrigins("https://localhost:3000")
-//                .allowedHeaders("*")
-//                .allowedMethods("*");
-//    }
-//}
+package Doffy.server.test.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOrigins("https://localhost:3000")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
+}
