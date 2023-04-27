46e217dd99ac1011af814e7404b3cecb64693d8a
Goomt <serosm93@gmail.com>
2023-04-27 22:17:02 +0900

    swagger test

--- packages/server/src/main/java/Doffy/server/community/controller/BoardController.java
+++ packages/server/src/main/java/Doffy/server/community/controller/BoardController.java
@@ -5,6 +5,9 @@ import Doffy.server.community.dto.board.BoardResponseDto;
 import Doffy.server.community.entity.Board;
 import Doffy.server.community.mapper.BoardMapper;
 import Doffy.server.community.service.BoardService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -20,12 +23,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/community")
 @Slf4j
 @RequiredArgsConstructor
+@Api(value = "Community API")
 public class BoardController {
     private final BoardService boardService;
     private final BoardMapper boardMapper;
 
+    @ApiOperation(value = "Create a new board", response = BoardResponseDto.class)
     @PostMapping
-    public ResponseEntity<BoardResponseDto> createBoard(@RequestBody BoardPostDto boardPostDto) {
+    public ResponseEntity<BoardResponseDto> createBoard(
+            @ApiParam(value = "Board information", required = true) @RequestBody BoardPostDto boardPostDto) {
         Board board = boardService.createBoard(boardPostDto);
         BoardResponseDto response = boardMapper.toBoardResponseDto(board);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
