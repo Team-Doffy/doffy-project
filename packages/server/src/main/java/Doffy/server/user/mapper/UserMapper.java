7097e9961f191ebd64f991050cf8d62e9e3d1e9b
CHOHYUNHWA <gusghk115@gmail.com>
2023-04-28 12:49:01 +0900

    fix : TestController 주석처리, Mapper Unmapping 경고 ignore 처리

--- packages/server/src/main/java/Doffy/server/user/mapper/UserMapper.java
+++ packages/server/src/main/java/Doffy/server/user/mapper/UserMapper.java
@@ -3,8 +3,10 @@ package Doffy.server.user.mapper;
 import Doffy.server.user.dto.UserDto;
 import Doffy.server.user.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
 
     User sighUpToUser(UserDto.SignUp requestBody);
