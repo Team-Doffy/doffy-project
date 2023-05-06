00d8cc8c64dc62eb51fd0d30278a5463ab668630
string_main <yhj2756@naver.com>
2023-05-07 01:35:32 +0900

    feat: 공식 문서 카드뷰 CSS 수정

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -5,22 +5,21 @@ import React from "react";
 function Main() {
   return (
     <>
-      <div className="w-64 h-48 mx-10 my-10 rounded overflow-hidden border-solid border border-gray-300">
-        <div className="h-full flex flex-col items-center justify-center">
-          <div className="flex items-start">
-            <div>
-              <div className="p-3 border rounded-full mr-12">
-                <Image
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png"
-                  alt="docs_logo"
-                  width={28}
-                  height={28}
-                />
-              </div>
-              <p className="text-gray-600 text-base">React.js</p>
-            </div>
+      <div className="w-64 h-48 mx-10 my-10 rounded-lg overflow-hidden border-solid border border-gray-300">
+        <div className="mt-6 flex flex-col items-center">
+          <div className="pr-6 w-full flex justify-end">
             <Image src="/icons/star1.svg" alt="star" width={25} height={24} />
           </div>
+
+          <div className="p-2 border rounded-full">
+            <Image
+              src="/icons/React-icon.svg"
+              alt="docs_logo"
+              width={28}
+              height={28}
+            />
+          </div>
+          <p className="pt-5 text-gray-600 text-base">React.js</p>
         </div>
       </div>
     </>
