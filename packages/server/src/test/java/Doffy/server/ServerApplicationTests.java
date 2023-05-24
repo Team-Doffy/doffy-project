d80c3f61888e6f6c8cd0b0fb3d331ec0f4196efd
CHOHYUNHWA <gusghk115@gmail.com>
2023-05-24 17:41:51 +0900

    fix : test deploy

--- packages/server/src/test/java/Doffy/server/ServerApplicationTests.java
+++ packages/server/src/test/java/Doffy/server/ServerApplicationTests.java
@@ -7,8 +7,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-//@SpringBootTest
-//@EnableJpaAuditing
+@SpringBootTest
+@EnableJpaAuditing
 class ServerApplicationTests {
 	@Test
 	void contextLoads() {
