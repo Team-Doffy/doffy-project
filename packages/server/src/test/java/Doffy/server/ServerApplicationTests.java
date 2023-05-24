62cf039f93a1262a49de496d89c328ecd9b5fd3b
HyunHwa Cho <gusghk115@gmail.com>
2023-05-25 01:29:52 +0900

    Feat/user (#37)
    * test: deploy test
    
    * test : user 테스트 코드 주석처리
    
    * fix : 환경변수 파일 삭제
    
    * fix : test code comment out
    
    * fix : test code 수정
    
    * fix : test code
    
    * fix : test deploy
    
    * fix:application.yml
    
    * test: application.yml create
    
    * Create application.yml
    
    * fix: application.yml

--- packages/server/src/test/java/Doffy/server/ServerApplicationTests.java
+++ packages/server/src/test/java/Doffy/server/ServerApplicationTests.java
@@ -1,15 +1,19 @@
 package Doffy.server;
 
+import Doffy.server.security.auth.jwt.JwtTokenizer;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-//@SpringBootTest
-//@EnableJpaAuditing
+@SpringBootTest
+@EnableJpaAuditing
 class ServerApplicationTests {
+
 	@Test
 	void contextLoads() {
 	}
--- packages/server/src/test/java/Doffy/server/community/repository/ReplyRepositoryTest.java
+++ packages/server/src/test/java/Doffy/server/community/repository/ReplyRepositoryTest.java
@@ -1,32 +1,32 @@
-package Doffy.server.community.repository;
-
-import Doffy.server.community.entity.Board;
-import Doffy.server.community.entity.Reply;
-import Doffy.server.user.entity.User;
-import Doffy.server.user.repository.UserRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@DataJpaTest
-public class ReplyRepositoryTest {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private BoardRepository boardRepository;
-
-    @Autowired
-    private ReplyRepository replyRepository;
-
+//package Doffy.server.community.repository;
+//
+//import Doffy.server.community.entity.Board;
+//import Doffy.server.community.entity.Reply;
+//import Doffy.server.user.entity.User;
+//import Doffy.server.user.repository.UserRepository;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.PageRequest;
+//import org.springframework.data.domain.Pageable;
+//
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@DataJpaTest
+//public class ReplyRepositoryTest {
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Autowired
+//    private BoardRepository boardRepository;
+//
+//    @Autowired
+//    private ReplyRepository replyRepository;
+//
 //    @Test
 //    public void testFindByUser() {
 //        // given
@@ -39,29 +39,29 @@ public class ReplyRepositoryTest {
 //        // then
 //        assertNotNull(replies);
 //    }
-
-    @Test
-    public void testFindAll() {
-        // given
-        Pageable pageable = PageRequest.of(0, 10);
-
-        // when
-        Page<Reply> replies = replyRepository.findAll(pageable);
-
-        // then
-        assertNotNull(replies);
-    }
-
-    @Test
-    public void testFindByBoard() {
-        // given
-        Board board = new Board();
-        boardRepository.save(board);
-
-        // when
-        List<Reply> replies = replyRepository.findByBoard(board);
-
-        // then
-        assertNotNull(replies);
-    }
-}
+//
+//    @Test
+//    public void testFindAll() {
+//        // given
+//        Pageable pageable = PageRequest.of(0, 10);
+//
+//        // when
+//        Page<Reply> replies = replyRepository.findAll(pageable);
+//
+//        // then
+//        assertNotNull(replies);
+//    }
+//
+//    @Test
+//    public void testFindByBoard() {
+//        // given
+//        Board board = new Board();
+//        boardRepository.save(board);
+//
+//        // when
+//        List<Reply> replies = replyRepository.findByBoard(board);
+//
+//        // then
+//        assertNotNull(replies);
+//    }
+//}
